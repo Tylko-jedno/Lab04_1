1d7d18e2f16dab48fa2df7c3a21d270bdaa27ee0
Tylko-jedno <diogkek@gmail.com>
2024-09-21 15:53:08 +0300

    first master commit

---
+++ Lab04_1/Lab04_1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double N; //вхідні дані
+	double S, k;
+
+	cout << "N = "; cin >> N;
+
+	S = 0;
+	k = N;
+	while (k <= 19){
+		S += sqrt(sin(1.*k) * sin(1. * k) + cos(1. * (N / k)) * cos(1. * (N / k)));
+		k++;
+	}
+	cout << S << endl;
+
+	S = 0;
+	k = N;
+	do{
+		S += sqrt(sin(1. * k) * sin(1. * k) + cos(1. * (N / k)) * cos(1. * (N / k)));
+		k++;
+	} while (k <= 19);
+	cout << S << endl;
+
+	S = 0;
+	for (k = N; k <= 19; k++) {
+		S += sqrt(sin(1. * k) * sin(1. * k) + cos(1. * (N / k)) * cos(1. * (N / k)));
+
+	}
+	cout << S << endl;
+
+	S = 0;
+	for (k = 19; k >= N; k--) {
+		S += sqrt(sin(1. * k) * sin(1. * k) + cos(1. * (N / k)) * cos(1. * (N / k)));
+
+	}
+	cout << S << endl;
+}
